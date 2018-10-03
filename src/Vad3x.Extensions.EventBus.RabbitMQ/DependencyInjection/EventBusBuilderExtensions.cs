cca92b6d2c5011219b4a58c32dd2f8df5faae05f
Vadim Orekhov <vad3xs@gmail.com>
2018-10-03 10:51:48 +0100

    fixed error

--- src/Vad3x.Extensions.EventBus.RabbitMQ/DependencyInjection/EventBusBuilderExtensions.cs
+++ src/Vad3x.Extensions.EventBus.RabbitMQ/DependencyInjection/EventBusBuilderExtensions.cs
@@ -51,40 +51,5 @@ namespace Microsoft.Extensions.DependencyInjection
 
             return builder;
         }
-
-        public static IEventBusBuilder AddSubscriber<TEvent, THandler>(
-            this IEventBusBuilder builder,
-            string exchangeName,
-            string queueName)
-            where TEvent : IntegrationEvent
-            where THandler : class, IIntegrationEventHandler<TEvent>
-        {
-            if (builder == null)
-            {
-                throw new ArgumentNullException(nameof(builder));
-            }
-
-            if (exchangeName == null)
-            {
-                throw new ArgumentNullException(nameof(exchangeName));
-            }
-
-            if (queueName == null)
-            {
-                throw new ArgumentNullException(nameof(queueName));
-            }
-
-            builder.Services.AddTransient<THandler>();
-
-            builder.Services.AddSingleton(new SubscriberInfo
-            {
-                EventType = typeof(TEvent),
-                HandlerType = typeof(THandler),
-                ExchangeName = exchangeName,
-                QueueName = queueName
-            });
-
-            return builder;
-        }
     }
 }
